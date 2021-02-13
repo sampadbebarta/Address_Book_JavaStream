043fd1b892feb39a4c369ab76503709c285f9504
Sampad <bebartasampad@gmail.com>
2021-02-13 16:33:39 +0530

    Creation of contacts in Address Book using Java Stream API

---
+++ AddressBookMain.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Address Book Management System using Java Stream ");
+    }
+
+}
