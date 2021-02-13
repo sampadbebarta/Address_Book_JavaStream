abb08e029bdef0d43c6d8c9c698fc41004e48937
Sampad <bebartasampad@gmail.com>
2021-02-13 17:27:31 +0530

    No Duplicate Entry of Same Person in Address Book using Java Stream API

--- AddressBook.java
+++ AddressBook.java
@@ -1,21 +1,11 @@
-import java.util.ArrayList;
-
-public class AddressBook {
-
-	public ArrayList<ContactDetails> contactList = new ArrayList<>();
-
-	public  ArrayList<ContactDetails> contactList(ContactDetails cd){
-		contactList.add(cd);
-		return contactList;
-	}
-
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class AddressBook {
 	public static Scanner sc = new Scanner(System.in);
 	public ArrayList<ContactDetails> contactList = new ArrayList<>();
 
-	public void addContactDetails(){
+	public void addContactDetails() {
 		System.out.println("Enter the contact details:");
 		String firstName = sc.next();
 		String lastName = sc.next();
@@ -30,13 +20,10 @@ public class AddressBook {
 
 	}
 
-	public boolean editContactDetails(String Name)
-	{
+	public boolean editContactDetails(String Name) {
 		int flag = 0;
-		for(ContactDetails contact: contactList)
-		{
-			if(contact.getFirstName().equals(Name))
-			{
+		for (ContactDetails contact : contactList) {
+			if (contact.getFirstName().equals(Name)) {
 				System.out.println("Enter the detail which needs to be updated:");
 
 				System.out.println("1 : First Name of the contact to be edited");
@@ -51,74 +38,85 @@ public class AddressBook {
 				System.out.println("Select the index for the contact detail you want to edit ");
 
 				int choice = sc.nextInt();
-				switch(choice)
-				{
-				case 1:
-				{
+				switch (choice) {
+				case 1 -> {
 					System.out.println("Enter First Name: ");
 					String firstName = sc.next();
 					contact.setFirstName(firstName);
 					break;
 				}
-				case 2:
-				{
+				case 2 -> {
 					System.out.println("Enter last name: ");
 					String lastName = sc.next();
 					contact.setLastName(lastName);
 					break;
 				}
-				case 3:
-				{
+				case 3 -> {
 					System.out.println("Enter Address: ");
 					String address = sc.next();
 					contact.setAddress(address);
 					break;
 				}
-				case 4:
-				{
+				case 4 -> {
 					System.out.println("Enter City: ");
 					String city = sc.next();
 					contact.setCity(city);
 					break;
 				}
-				case 5:
-				{
+				case 5 -> {
 					System.out.println("Enter State: ");
-					String state =sc.next();
+					String state = sc.next();
 					contact.setState(state);
 					break;
 				}
-				case 6:
-				{
+				case 6 -> {
 					System.out.println("Enter Email: ");
 					String email = sc.next();
 					contact.setZip(email);
 					break;
 				}
-				case 7:
-				{
+				case 7 -> {
 					System.out.println("Enter Phone Number:");
 					String phoneNumber = sc.next();
 					contact.setPhoneNumber(phoneNumber);
 					break;
 				}
-				case 8:
-				{
+				case 8 -> {
 					System.out.println("Enter Zip Code: ");
 					String zip = sc.next();
 					contact.setZip(zip);
 					break;
 				}
-
 				}
 
 				flag = 1;
 				break;
 			}
 		}
-		if(flag==1)
-			return true;
-		else
-			return false;
+		return flag == 1;
+	}
+
+
+	public boolean deleteContact(String name) {
+		int flag = 0;
+		for (ContactDetails contact : contactList) {
+			if (contact.getFirstName().equals(name)) {
+				contactList.remove(contact);
+				flag = 1;
+				break;
+			}
+		}
+		return flag == 1;
+	}
+
+	public void checkDuplicate() {
+		Set<String> ContactSet = new HashSet<>();
+		Set<ContactDetails> filterSet = contactList.stream().filter(n -> !ContactSet.add(n.getFirstName())).collect(Collectors.toSet());
+
+		for (ContactDetails contact : filterSet) {
+			System.out.println("The Duplicate Contact is: " + contact.getFirstName() + " " + contact.getLastName());
+		}
+
+
 	}
 }
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,32 +1,10 @@
 import java.util.*;
 class AddressBookMain {
 	public static Scanner sc = new Scanner(System.in);
-	public static void main(String[] args) {
-		System.out.println("Welcome to the Address Book Management System using Java Stream ");
-		AddressBook addressBook = new AddressBook();
-		System.out.println("Enter the contact details:");
-		String firstName = sc.next();
-		String lastName = sc.next();
-		String address = sc.next();
-		String city = sc.next();
-		String state = sc.next();
-		String email = sc.next();
-		String phoneNumber = sc.next();
-		String zip = sc.next();
-		ContactDetails cd = new ContactDetails(firstName, lastName, address, city, state, email, phoneNumber, zip);
-
-		ArrayList<ContactDetails> contactList = addressBook.contactList(cd);
-		for(ContactDetails contact:contactList){
-			System.out.println(contact.getFirstName()+" "+contact.getLastName()+" "+contact.getAddress()+" "+contact.getCity()+
-					" "+contact.getState()+" "+contact.getEmail()+" "+contact.getPhoneNumber()+" "+contact.getZip());
-		}
-
-
-	}
 	private static AddressBook addressBook = new AddressBook();
+	public Map<String,AddressBook> addressBookListMap = new HashMap<>();
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to the Address Book Management System using Java Stream");
+	public void addAddressBook(String addressBookName){
 		AddressBookMain addBookMain = new AddressBookMain();
 		boolean flag = true;
 
@@ -34,7 +12,8 @@ class AddressBookMain {
 
 			System.out.println("1.Add Contact");
 			System.out.println("2.Edit Contact");
-			System.out.println("3.Exit");
+			System.out.println("3.Delete");
+			System.out.println("4.Exit");
 			System.out.println("Enter Choice: ");
 
 			int option = sc.nextInt();
@@ -42,27 +21,85 @@ class AddressBookMain {
 			switch (option)
 			{
 			case 1:
-				addressBook.addContactDetails();
+				System.out.println("Enter the number of Contacts to be added");
+				int noOfContacts = sc.nextInt();
+				for(int i = 0; i < noOfContacts; i++) {
+					addressBook.addContactDetails();
+				}
 				break;
 
 			case 2:
 				System.out.println("Enter the Person First name to edit details: ");
-				String person_name = sc.next();
+				String personName = sc.next();
 
-				boolean b = addressBook.editContactDetails(person_name);
-				if (b == true) {
-					System.out.println("Details Updated");
+				boolean listEdited = addressBook.editContactDetails(personName);
+				if (listEdited) {
+					System.out.println("List Edited Successfully");
 				} else {
-					System.out.println("Contact Not Found");
+					System.out.println("List Cannot be Edited");
 				}
 				break;
 
 			case 3:
+				System.out.println("Enter the Contact to be deleted:");
+				String firstName = sc.next();
+				boolean listDeleted = addressBook.deleteContact(firstName);
+				if (listDeleted) {
+					System.out.println("Deleted Contact from the List");
+				} else {
+					System.out.println("List Cannot be Deleted");
+				}
+				break;
+
+			case 4:
 				flag =false;
 				break;
 
 			}
+			addressBookListMap.put(addressBookName, addressBook);
+			System.out.println("Address Book Added Successfully");
 		}
+
+	}
+
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Address Book Management System using Java Stream");
+		AddressBookMain addressBookMain = new AddressBookMain();
+		boolean flag =true;
+		while(flag)
+		{
+			System.out.println("1.Add New Address Book");
+			System.out.println("2.Find Duplicate Entry in Address Book");
+			System.out.println("3.Exit");
+			System.out.println("Enter choice: ");
+			int option = sc.nextInt();
+			switch (option){
+			case 1:
+				System.out.println("Enter the Name of Address Book: ");
+				String addressBookName = sc.next();
+				if(addressBookMain.addressBookListMap.containsKey(addressBookName)){
+					System.out.println("The Address book Already Exists");
+					break;
+				}else {
+					addressBookMain.addAddressBook(addressBookName);
+					break;
+				}
+
+			case 2:
+				for (Map.Entry<String, AddressBook> entry : addressBookMain.addressBookListMap.entrySet()) {
+					AddressBook value = entry.getValue();
+					System.out.println("Address Book Name: " + entry.getKey());
+					value.checkDuplicate();
+				}
+
+			case 3:
+				flag = false;
+				break;
+
+			}
+		}
+
 
 	}
 }
